dbf9f5480e0a2272a94b245adae9148f97ae3171
Mengkhey45 <mengkheykhorn8@gmail.com>
2025-09-06 14:54:57 +0700

    fix import models

--- routes/quoteRoutes.js
+++ routes/quoteRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Quote = require("../models/Quote");
+const Quote = require("../models/quote");
 
 router.get("/random", async(req,res) =>{
 try{
