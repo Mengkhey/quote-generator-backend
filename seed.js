646ddbead9efc0671debe75c59de53d2dd61f765
Mengkhey45 <mengkheykhorn8@gmail.com>
2025-09-10 19:35:39 +0700

    fix bug and make it organize

--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const Quote = require("./models/Quote");
+const Quote = require("../models/Quote");
 
 mongoose.connect(process.env.MONGODB_URI);
 
